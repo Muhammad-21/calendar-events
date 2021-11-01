e018730173ca873756a61df26d5e88f8c5569932
Muhammad <muhammadmahmudov21@yandex.ru>
2021-11-01 19:05:09 +0300

    Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,15 @@
+import { Card, Layout, Row } from 'antd'
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 export default function Login() {
     return (
-        <div>
-            Login
-        </div>
+        <Layout>
+            <Row justify="center" align="middle" className="h100">
+                <Card>
+                    <LoginForm/>
+                </Card>
+            </Row>
+        </Layout>
     )
 }
