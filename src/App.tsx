e2c678eb5abb52eb4f660227b810fbebee4d9eb2
Muhammad <muhammadmahmudov21@yandex.ru>
2021-11-01 19:05:36 +0300

    added hooks and reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Layout } from 'antd';
+import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/User';
 
 function App() {
+  const {setUser,setIsAuth} = useActions();
+  useEffect(() => {
+    if(localStorage.getItem('auth')){
+      setUser({username:localStorage.getItem('username' || '')} as IUser)
+      setIsAuth(true);
+    }
+  }, )
+  
   return (
     <Layout>
       <Navbar/>
