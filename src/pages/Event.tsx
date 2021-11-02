ff64300ee520d26072da2af984f4b26721bc45cc
Muhammad <muhammadmahmudov21@yandex.ru>
2021-11-02 21:23:09 +0300

    [+] event calendar

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button, Layout, Modal, Row } from 'antd'
+import React, { useState } from 'react'
+import EventCalendar from '../components/EventCalendar'
 
 export default function Event() {
+    const [modalVisible,setModalVisible] = useState(false);
     return (
-        <div>
-           Event Page 
-        </div>
+        <Layout>
+           <EventCalendar events={[]}/>
+           <Row justify="center">
+               <Button onClick={()=>setModalVisible(true)}>Добавить события</Button>
+           </Row>
+           <Modal
+            title="Добавить события"
+            visible={modalVisible}
+            footer={null}
+            onCancel={()=>setModalVisible(false)}
+           >
+
+           </Modal>
+        </Layout>
     )
 }
